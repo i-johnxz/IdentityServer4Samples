23cb61919e4c69e92ac0c540622d0109c4ef3b0b
xiongz <a824440331@gmail.com>
2019-10-18 11:02:43 +0800

    update code

---
+++ src/IdentityServer4Samples/1_ClientCredentials/Api/Apis/Controllers/IdentityController.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Apis.Controllers
+{
+    [Authorize]
+    [Route("identity")]
+    public class IdentityController : ControllerBase
+    {
+        public IActionResult Get()
+        {
+            return new JsonResult(from c in User.Claims select new {c.Type, c.Value});
+        }
+    }
+}
