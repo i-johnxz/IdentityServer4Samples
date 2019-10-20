8a1fd7a7d627f9cffa3004b6bfc3c0e4587c19c0
xiongz_home <xiongz@DESKTOP-SVDIFJE>
2019-10-20 20:57:06 +0800

    update code

---
+++ src/IdentityServer4Samples/5_HybridFlowAuthenticationWithApiAccess/Client/Client5/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+
+namespace Client5
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // discover endpoints from metadata
+            var client = new HttpClient();
+            //http://localhost:44303
+            
+            var disco = await client.GetDiscoveryDocumentAsync("http://localhost:44303");
+
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+                return;
+            }
+            
+            // request token
+            var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Address = disco.TokenEndpoint,
+                ClientId = "client",
+                ClientSecret = "secret",
+
+                Scope = "api1"
+            });
+
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+            Console.WriteLine("\n\n");
+            
+            // call api
+            var apiClient = new HttpClient();
+            apiClient.SetBearerToken(tokenResponse.AccessToken);
+            
+            
+            var response = await apiClient.GetAsync("http://localhost:54235/identity");
+
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+            }
+            else
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content));
+            }
+
+            // api host 54235
+
+        }
+    }
+}
--- src/IdentityServer4Samples/5_HybridFlowAuthenticationWithApiAccess/MvcClient/MvcClient5/Controllers/HomeController.cs
+++ src/IdentityServer4Samples/5_HybridFlowAuthenticationWithApiAccess/MvcClient/MvcClient5/Controllers/HomeController.cs
@@ -48,7 +48,7 @@ namespace MvcClient5.Controllers
 
             var client = new HttpClient();
             client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
-            var content = await client.GetStringAsync("http://localhost:5001/identity");
+            var content = await client.GetStringAsync("http://localhost:54235/identity");
 
             ViewBag.Json = JArray.Parse(content).ToString();
             return View("json");
